094b1a46792b643d7e683798b7e34a7c58af9e14
Hamza Alvi <42583422+hamzaalvi1@users.noreply.github.com>
2020-07-25 11:07:45 +0500

    Practice string assignment

---
+++ Strings/practice.js
@@ -0,0 +1,34 @@
+//Reverse String
+let str = "The quick brown fox jumps over lazy dog"
+const reverseStr = (string)=>{
+ const reverseStr  = string.split("").reverse().join("")
+ return reverseStr 
+}
+
+console.log(reverseStr(str))
+console.log(reverseStr("Hamza Alvi"))
+//Alternate
+function reverseStr2(string){
+let revStr = ""
+for(let i = string.length - 1 ; i >= 0 ; i--){
+    revStr+= string[i]
+}
+return revStr
+
+}
+console.log(reverseStr2("Zebra"))
+
+// clone indexOf
+let cloneIndexof = (str,indexWord)=>{
+ let splitStr = str.split("")
+ const filterStr = splitStr.map((string,index)=>{
+     const _index = string === indexWord ? [index] : []  
+   return   _index
+   
+ })
+ const mergeIndex = filterStr.flat()
+ const indexArr =  mergeIndex.length !== 0 ? mergeIndex.toString() : "-1" 
+  let  finalIndex = indexArr.slice(0,1)
+  return finalIndex 
+} 
+console.log(cloneIndexof("Hamza","H"))
